dc425b966650694709fd8ce568c10904edc7f165
DinhQuy <hdquy2003@gmail.com>
2025-11-25 13:45:47 +0700

    fix search supervisors-activity

--- app/src/main/java/com/example/testui/activities/SupervisorsActivity.java
+++ app/src/main/java/com/example/testui/activities/SupervisorsActivity.java
@@ -2,6 +2,8 @@ package com.example.testui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 
@@ -24,11 +26,15 @@ import com.example.testui.model.Teacher;
 import com.example.testui.untilities.Constants;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SupervisorsActivity extends AppCompatActivity {
     ActivitySupervisorsBinding binding;
     GVHDViewModel gvhdViewModel;
+    List<Teacher> listTeacher = new ArrayList<>();
+    List<Teacher> listTeacherDisplay = new ArrayList<>();
+    TeacherAdapter teacherAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,38 +47,74 @@ public class SupervisorsActivity extends AppCompatActivity {
             return insets;
         });
 
+        init();
+        setupRecyclerView();
+        loadData();
+        observeData();
+        event();
+    }
+
+    void init() {
         gvhdViewModel = new ViewModelProvider(this, new GVHDViewModelFactory(this)).get(GVHDViewModel.class);
-//        gvhdViewModel.getGVHD();
-//        gvhdViewModel.getListSupervisor().observe(this, result -> {
-//            List<Supervisor> listSupervisor = result;
-//            binding.tvCountSupervisor.setText(String.valueOf(listSupervisor.size()));
-//            Log.d("Supervisor", result.size() + "");
-//            GVHDAdapter gvhdAdapter = new GVHDAdapter(this, listSupervisor, position -> {
-//                Intent intent = new Intent(this, SupervisorActivity.class);
-//                Gson gson = new Gson();
-//                Supervisor supervisor = listSupervisor.get(position);
-//                String strGVHD = gson.toJson(supervisor);
-//                intent.putExtra(Constants.KEY_SUPERVISOR, strGVHD);
-//                startActivity(intent);
-//            });
-//            binding.rvGiangVien.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-//            binding.rvGiangVien.setAdapter(gvhdAdapter);
-//        });
+    }
+
+    void loadData() {
         gvhdViewModel.loadTeacher();
+    }
+
+    void setupRecyclerView() {
+        binding.rvGiangVien.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+         teacherAdapter = new TeacherAdapter(this, new ArrayList<>(), position -> {
+            Intent intent = new Intent(this, SupervisorActivity.class);
+            Gson gson = new Gson();
+            Teacher teacher = listTeacher.get(position);
+            String strTeacher = gson.toJson(teacher);
+            intent.putExtra(Constants.KEY_TEACHER, strTeacher);
+            startActivity(intent);
+        });
+        binding.rvGiangVien.setAdapter(teacherAdapter);
+    }
+
+    void  observeData() {
         gvhdViewModel.getTeachers().observe(this, result -> {
-            List<Teacher> listTeacher = result;
+            listTeacher = result;
             binding.tvCountSupervisor.setText(String.valueOf(listTeacher.size()));
             Log.d("Teacher", result.size() + "");
-            TeacherAdapter teacherAdapter = new TeacherAdapter(this, listTeacher, position -> {
-                Intent intent = new Intent(this, SupervisorActivity.class);
-                Gson gson = new Gson();
-                Teacher teacher = listTeacher.get(position);
-                String strTeacher = gson.toJson(teacher);
-                intent.putExtra(Constants.KEY_TEACHER, strTeacher);
-                startActivity(intent);
-            });
-            binding.rvGiangVien.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-            binding.rvGiangVien.setAdapter(teacherAdapter);
+            teacherAdapter.updateData(listTeacher);
         });
+    }
+
+    void event() {
+        binding.etSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                filter(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    void filter(String keyWord) {
+        listTeacherDisplay.clear();
+        if (keyWord.isEmpty()) {
+            listTeacherDisplay.addAll(listTeacher);
+        } else {
+            for (Teacher teacher : listTeacher) {
+                if (teacher.getUser().getFullname().toLowerCase().contains(keyWord.toLowerCase())) {
+                    listTeacherDisplay.add(teacher);
+                }
+            }
+        }
+        binding.tvCountSupervisor.setText(String.valueOf(listTeacherDisplay.size()));
+        teacherAdapter.updateData(listTeacherDisplay);
     }
 }
